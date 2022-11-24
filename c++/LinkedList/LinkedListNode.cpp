84cca8d8b965c503144ea2f32a8e984c8cd088df
Mart <phantichchai1998@gmail.com>
2022-11-24 21:05:10 +0700

    add c++ data structure and backtracking word search

---
+++ c++/LinkedList/LinkedListNode.cpp
@@ -0,0 +1,17 @@
+class LinkedListNode
+{
+private:
+    int data;
+    LinkedListNode* next;
+public:
+    LinkedListNode(/* args */);
+    ~LinkedListNode();
+};
+
+LinkedListNode::LinkedListNode(/* args */)
+{
+}
+
+LinkedListNode::~LinkedListNode()
+{
+}
---
+++ c++/Tree/BinaryTreeNode.cpp
@@ -0,0 +1,19 @@
+class BinaryTreeNode
+{
+private:
+    /* data */
+    int data;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+public:
+    BinaryTreeNode(/* args */);
+    ~BinaryTreeNode();
+};
+
+BinaryTreeNode::BinaryTreeNode(/* args */)
+{
+}
+
+BinaryTreeNode::~BinaryTreeNode()
+{
+}
