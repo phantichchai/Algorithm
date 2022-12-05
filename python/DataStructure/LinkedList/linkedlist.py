26ac8db00ae7cd02f51a614e2f7226b5a5d0cbc6
Mart <phantichchai1998@gmail.com>
2022-12-05 12:46:11 +0700

    add new method in linked list

--- python/DataStructure/LinkedList/linkedlist.py
+++ python/DataStructure/LinkedList/linkedlist.py
@@ -3,13 +3,6 @@ class ListNode:
         self.val = val
         self.next = None
 
-    def __repr__(self) -> str:
-        repr = str(self.val) + "->"
-        while self.next:
-            repr += str(self.next.val) + "->"
-            self.next = self.next.next
-        return repr
-
     def deleteNode(self, node) -> None:
         node.val = node.next.val
         node.next = node.next.next
@@ -17,10 +10,31 @@ class ListNode:
     def createNextNode(self, node) -> None:
         self.next = node
 
+def representList(head: ListNode) -> str:
+        repr = ""
+        while head:
+            repr += str(head.val) + "->"
+            head = head.next
+        return repr
+
+def middleNode(head: ListNode) -> ListNode:
+        # create two pointer that point to head slow and fast
+        slow, fast = head, head
+        # iterate until fast and fast.next not equal None mean the list has no more next node
+        while fast and fast.next:
+            # update current slow by move one step each time
+            slow = slow.next
+            # update current fast by move two steps each time
+            fast = fast.next.next
+            
+        # return current slow which is a middle node
+        return slow    
+
 if __name__ == "__main__":
     node = ListNode(0)
     linked_list = node
     for i in range(1, 10):
         node.next = ListNode(i)
         node = node.next
-    print(linked_list)
+    print(representList(linked_list))
+    print(f"Mid node of this linked list is {middleNode(linked_list).val}")
