07e10cceda67c09196ec08e5b4065df7e124f47d
Mart <phantichchai1998@gmail.com>
2022-12-19 02:14:20 +0700

    739. Daily Temperatures stack problem

--- python/Algorithms/Stack/stack.py
+++ python/Algorithms/Stack/stack.py
@@ -24,3 +24,25 @@ class Solution:
             stack.append(i)
             
         return sum_of_minimums % MOD
+
+    # 739. Daily Temperatures
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # create the 'days' stack for contain the day that not have greater temperature
+        # init the first day
+        days = [0]
+        # create the output equal to length of temperatures
+        output = [0]*len(temperatures)
+
+        # iterate to 1 to n 
+        for i in range(1, len(temperatures)):
+            # loop that the the 'days' stack is not empty and ith day temperature that greater than top of the 'days' stack 
+            while days and temperatures[i] > temperatures[days[-1]]:
+                # update the how difference between ith day and top of the 'days' stack
+                output[days[-1]] = i - days[-1]
+                # pop the top of the days stack
+                days.pop()
+            # add ith day temperature to the 'days' stack
+            days.append(i)
+        return output
